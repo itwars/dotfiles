7f37081421c5221c5396528725016cd587ab4130
vrabah <vrabah@spring-rolls.my.domain>
2024-03-17 19:03:14 +0100

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -479,8 +479,9 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = ".vimrc",
   callback = function(args)
     vim.cmd('! cp ~/.vimrc ~/dotfiles/.vimrc')
-    vim.cmd('! cd ~/dotfiles; git add .vimrc; git commit -m update; git push')
+    let test = vim.cmd('! cd ~/dotfiles; git add .vimrc; git commit -m update; git push')
     notify('File .vimrc push to git repository !',  notify.INFO, {title="Autocmd .vimrc"}) 
+    notify($test,  notify.INFO, {title="Autocmd .vimrc"}) 
   end,
   desc = "Github backup of .vimrc.",
 })
