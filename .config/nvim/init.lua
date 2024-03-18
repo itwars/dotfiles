decce47c61936691774fc9d72f1bec363caa4202
vrabah <vrabah@spring-rolls.my.domain>
2024-03-18 09:23:56 +0100

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -292,8 +292,8 @@ local null_ls = require('null-ls')
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.eslint,
     null_ls.builtins.formatting.golines,
---    null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.completion.spell,
   }
 })
