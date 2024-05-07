1b227fee364a82d854ab453e469855f73523bc62
itwars <vincent.rabah@gmail.fr>
2024-05-07 14:06:53 +0200

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -449,29 +449,6 @@ require 'key-menu'.set('n', '<leader>')
 --└───────────────┘
 require 'colorizer'.setup()
 
---┌────────────────────────────┐
---│ Colorful window separators │
---└────────────────────────────┘
-require("colorful-winsep").setup({
-  -- highlight for Window separator
-  hi = {
-    bg = "#ff0000",
-    fg = "#00ff00",
-  },
-  -- This plugin will not be activated for filetype in the following table.
-  no_exec_files = { "packer", "TelescopePrompt", "mason", "CompetiTest", "NvimTree" },
-  -- Symbols for separator lines, the order: horizontal, vertical, top left, top right, bottom left, bottom right.
-  symbols = { "━", "┃", "┏", "┓", "┗", "┛" },
-  -- Smooth moving switch
-  smooth = true,
-  exponential_smoothing = true,
-  anchor = {
-    left = { height = 1, x = -1, y = -1 },
-    right = { height = 1, x = -1, y = 0 },
-    up = { width = 0, x = -1, y = 0 },
-    bottom = { width = 0, x = 1, y = 0 },
- },
-})
 --┌────────────────────────────────────────────────────────┐
 --│ Function to draw box around text when pressing <F4> key│
 --└────────────────────────────────────────────────────────┘
