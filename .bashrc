5f428e6b1cc287c153394a970a21261afa4a7786
Vincent Rabah <vincent.rabah@francetravail.fr>
2024-06-10 09:26:58 +0200

    update

--- .bashrc
+++ .bashrc
@@ -6,6 +6,8 @@ case $- in
 	  *) return;;
 esac
 
+if [ -f /home/ivra0940/.local/share/pipx/venvs/liftcli/shell/bash/bashrc ]; then source /home/ivra0940/.local/share/pipx/venvs/liftcli/shell/bash/bashrc; fi
 for i in ~/.bashrc.d/*.sh; do
 	. "$i"
 done; unset i 
+export PATH=$PATH:/snap/bin
--- .bashrc.d/00exports.sh
+++ .bashrc.d/00exports.sh
@@ -1,22 +1,12 @@
 export LC_NUMERIC="fr_FR.UTF-8"
-export GOPATH=/Users/vrabah/Documents/projects/lang/golang
+export GOPATH=$HOME/Documents/projects/lang/golang
 
-export PATH=$PATH:~/scripts:/Users/vrabah/gocode/bin/
-export PATH=$PATH:/usr/local/bin/brew/
+export PATH=$PATH:~/scripts:$HOME/ivra0940/gocode/bin/
 export PATH=$PATH:/sbin/
-export PATH=/usr/local/opt/ruby/bin:$PATH
-export PATH=/usr/local/sbin:$PATH
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$NPM_PACKAGES/bin
-export PATH=/usr/local/bin:$PATH:/usr/texbin
-export PATH=/usr/local/opt/openssl/bin:$PATH
-export PATH=$HOME/Library/Python/3.9/bin/:$PATH
-export PATH=/usr/local/opt/gnu-getopt/bin:$PATH
-export PATH=/usr/local/opt/curl/bin:$PATH
-export PATH=/Users/vrabah/Library/Python/3.10/bin:$PATH
-export PATH=/Users/vrabah/.cargo/bin:$PATH
+export PATH=$HOME/.cargo/bin:$PATH
 
-export HOMEBREWCASKOPTS="--appdir=/Applications"
 export COLORTERM=truecolor
 
 export _Z_OWNER=`whoami`
